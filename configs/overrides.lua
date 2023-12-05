5f4071429ee8c7f27a8e07c58a35a0bc71fc8f50
Bryant Paz <plbryant@hotmail.com>
2023-12-05 17:58:14 -0600

    feat: udpate cmp config

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -143,9 +143,15 @@ M.telescope = {
 }
 
 M.cmp = {
+
+	completion = {
+		completeopt = 'menu,menuone,noinsert,noselect',
+	},
+
 	mapping = {
 		['<A-Space>'] = require('cmp').mapping.complete(),
 		['<C-Space>'] = require('cmp').config.disable,
+		['<CR>'] = require('cmp').config.disable,
 	},
 
 	sources = {
