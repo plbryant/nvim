6d7c2f58452bbe80553243cc171fa3db3d920850
Bryant Paz <plbryant@hotmail.com>
2023-12-04 18:02:22 -0600

    feat: update oil plugin settings

--- configs/oil.lua
+++ configs/oil.lua
@@ -70,14 +70,14 @@ local options = {
   use_default_keymaps = true,
   view_options = {
     -- Show files and directories that start with "."
-    show_hidden = false,
+    show_hidden = true,
     -- This function defines what is considered a "hidden" file
     is_hidden_file = function(name, bufnr)
       return vim.startswith(name, '.')
     end,
     -- This function defines what will never be shown, even when `show_hidden` is set
     is_always_hidden = function(name, bufnr)
-      return false
+      return vim.startswith(name, '.git')
     end,
     sort = {
       -- sort order can be "asc" or "desc"
