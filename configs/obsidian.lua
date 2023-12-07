f40fff935122f168c5543c61af4b23add6413bd7
Bryant Paz <plbryant@hotmail.com>
2023-12-07 14:25:09 -0600

    fea: update obsidian path notes

--- configs/obsidian.lua
+++ configs/obsidian.lua
@@ -5,7 +5,7 @@ if not present then
 end
 
 local options = {
-	dir = '/home/bryant/Documents/github/obsidian/',
+	dir = '/home/bryant/Documents/github/notes/',
 	disable_frontmatter = true,
 	completion = {
 		nvim_cmp = true,
