616dc003017fa6667b4a94cfecee6cdcdcc9a008
Bryant Paz <plbryant@hotmail.com>
2024-01-02 22:06:25 -0600

    feat: change multicursor plugin

--- plugins.lua
+++ plugins.lua
@@ -76,6 +76,11 @@ local plugins = {
 		lazy = false,
 	},
 
+	{
+		'mg979/vim-visual-multi',
+		lazy = false,
+	},
+
 	-- lazy
 
 	{
@@ -290,28 +295,6 @@ local plugins = {
 		config = function()
 			require('custom.configs.hop')
 		end,
-	},
-
-	{
-		'smoka7/multicursors.nvim',
-		dependencies = { 'smoka7/hydra.nvim' },
-		opts = { hint_config = false },
-		cmd = {
-			'MCstart',
-			'MCvisual',
-			'MCclear',
-			'MCpattern',
-			'MCvisualPattern',
-			'MCunderCursor',
-		},
-		keys = {
-			{
-				mode = { 'v', 'n' },
-				'<C-n>',
-				'<cmd>MCstart<cr>',
-				desc = 'Create a selection for selected text or word under the cursor',
-			},
-		},
 	},
 
 	{
