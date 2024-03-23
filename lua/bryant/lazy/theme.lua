00882dea1e5698dd27b3d59d9997f93f4302918d
Bryant Paz <plbryant@hotmail.com>
2024-03-23 06:55:23 -0600

    chore: upadte rose pine file

---
+++ lua/bryant/lazy/theme.lua
@@ -0,0 +1,33 @@
+local themes = {
+	{
+		'bluz71/vim-moonfly-colors',
+		enabled = false,
+		name = 'moonfly',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme moonfly]])
+			vim.g.moonflyItalics = false
+		end,
+	},
+
+	{
+		'rose-pine/neovim',
+		enabled = true,
+		name = 'rose-pine',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('rose-pine').setup({
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = false,
+				},
+			})
+			vim.cmd([[colorscheme rose-pine]])
+		end,
+	},
+}
+
+return themes
