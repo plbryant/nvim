60aa7176950fc36a461068b8afaf15a90634bb34
Bryant Paz <pazbryant7@outlook.com>
2024-05-31 20:32:33 -0600

    chore: update trouble plugin config

--- lua/bryant/lazy/trouble.lua
+++ lua/bryant/lazy/trouble.lua
@@ -1,34 +1,40 @@
 return {
 	'folke/trouble.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
-		height = 20,
-		width = 55,
-		icons = true,
-		use_diagnostic_signs = true,
-		position = 'right',
+		auto_close = true,
+		focus = true,
 	},
+	cmd = 'Trouble',
 	keys = {
 		{
-			'<leader>t',
-			function()
-				require('trouble').toggle()
-			end,
+			'<leader>x',
+			'<cmd>Trouble diagnostics toggle<cr>',
+			desc = 'Diagnostics (Trouble)',
 		},
 		{
-			']t',
-			function()
-				require('trouble').next({ skip_groups = true, jump = true })
-			end,
+			'<leader>X',
+			'<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+			desc = 'Buffer Diagnostics (Trouble)',
 		},
 		{
-			'[t',
-			function()
-				require('trouble').previous({ skip_groups = true, jump = true })
-			end,
+			'<leader>cs',
+			'<cmd>Trouble symbols toggle focus=false<cr>',
+			desc = 'Symbols (Trouble)',
+		},
+		{
+			'<leader>cl',
+			'<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+			desc = 'LSP Definitions / references / ... (Trouble)',
+		},
+		{
+			'<leader>l',
+			'<cmd>Trouble loclist toggle<cr>',
+			desc = 'Location List (Trouble)',
+		},
+		{
+			'<leader>q',
+			'<cmd>Trouble qflist toggle<cr>',
+			desc = 'Quickfix List (Trouble)',
 		},
 	},
-	config = function(_, opts)
-		require('trouble').setup(opts)
-	end,
 }
